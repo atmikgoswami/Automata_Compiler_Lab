262eed7925ca864b2ff464f25e2856ea09837fe0
Atmik Goswami <135265480+atmikgoswami@users.noreply.github.com>
2024-10-29 00:08:43 +0530

    Update DFAminimization.cpp

--- Assignment_2/DFAminimization.cpp
+++ Assignment_2/DFAminimization.cpp
@@ -28,11 +28,30 @@ bool areEquivalent(int i, int j, const vector<vector<int>>& dfa, const vector<in
     return true;
 }
 
+vector<bool> findReachableStates(const vector<vector<int>>& dfa, int startState) {
+    int noOfStates = dfa.size();
+    vector<bool> reachable(noOfStates, false);
+    queue<int> q;
+    q.push(startState);
+    reachable[startState] = true;
+
+    while (!q.empty()) {
+        int state = q.front();
+        q.pop();
+        for (int next : dfa[state]) {
+            if (!reachable[next]) {
+                reachable[next] = true;
+                q.push(next);
+            }
+        }
+    }
+    return reachable;
+}
+
 vector<vector<int>> minimizeDFA(const vector<vector<int>>& dfa, const vector<int>& final_states, int noOfStates, int alphabetSize) {
     vector<int> partition(noOfStates, -1);
     set<int> final_set(final_states.begin(), final_states.end());
 
-    // Initial partition: final and non-final states are grouped separately
     int accept_idx = *final_set.begin(), reject_idx = -1;
     for (int i : final_set) {
         partition[i] = accept_idx;
@@ -44,7 +63,6 @@ vector<vector<int>> minimizeDFA(const vector<vector<int>>& dfa, const vector<int
         }
     }
 
-    // Iterative refinement of partition
     while (true) {
         vector<int> new_partition(noOfStates, -1);
         int i = 0;
@@ -56,7 +74,7 @@ vector<vector<int>> minimizeDFA(const vector<vector<int>>& dfa, const vector<int
                 if (partition[i] == partition[j] && areEquivalent(i, j, dfa, partition, alphabetSize)) {
                     new_partition[j] = i; 
                 } else if (i_next == noOfStates) {
-                    i_next = j; // Move to the first unmerged state
+                    i_next = j; 
                 }
             }
             i = i_next;
@@ -65,7 +83,6 @@ vector<vector<int>> minimizeDFA(const vector<vector<int>>& dfa, const vector<int
         partition = new_partition;
     }
 
-    // Create minimized DFA
     vector<vector<int>> minimizedDFA;
     map<int, int> state_map;
     int new_state = 0;
@@ -77,12 +94,9 @@ vector<vector<int>> minimizeDFA(const vector<vector<int>>& dfa, const vector<int
     }
 
     for (auto it:state_map) {
-
         int i = it.first;
-        //for state i
         vector<int> temp;
-        for(int j=0;j<alphabetSize;j++){
-
+        for(int j = 0; j < alphabetSize; j++) {
             int old_dest = dfa[i][j];
             int new_dest = state_map[partition[old_dest]];
             temp.push_back(new_dest);
@@ -93,44 +107,68 @@ vector<vector<int>> minimizeDFA(const vector<vector<int>>& dfa, const vector<int
     return minimizedDFA;
 }
 
-
-int main(){
+int main() {
     vector<vector<int>> dfa;
-	int noOfStates,alphabetSize,noOfFinalStates;
-	cout<<"Enter number of states in DFA : ";
-	cin>>noOfStates;
-	cout<<"Enter number of elements in alphabet : ";
-	cin>>alphabetSize;
+    int noOfStates, alphabetSize, noOfFinalStates;
+    cout << "Enter number of states in DFA : ";
+    cin >> noOfStates;
+    cout << "Enter number of elements in alphabet : ";
+    cin >> alphabetSize;
 
     cout << "Enter the alphabets one after another :\n";
     vector<char> alphabets(alphabetSize);
-    for (int i = 0; i < alphabetSize; i++){
+    for (int i = 0; i < alphabetSize; i++) {
         cin >> alphabets[i];
     }
 
-	cout<<"Enter number of final states : ";
-	cin>>noOfFinalStates;
-	vector<int> final_states(noOfFinalStates);
+    cout << "Enter number of final states : ";
+    cin >> noOfFinalStates;
+    vector<int> final_states(noOfFinalStates);
 
-	cout<<"Enter final state(s) : ";
-	for(int i=0;i<noOfFinalStates;i++){
-		cin>>final_states[i];
-	}
+    cout << "Enter final state(s) : ";
+    for (int i = 0; i < noOfFinalStates; i++) {
+        cin >> final_states[i];
+    }
 
-    for(int i=0;i<noOfStates;i++){
-		cout << "FOR STATE q" << i << endl;
-		vector<int> v(alphabetSize);
-		for(int j=0;j<alphabetSize;j++){
-			cout<<"Enter output state for input "<<alphabets[j]<<" : ";
-			cin>>v[j];
-		}
-		dfa.push_back(v);
-	}
+    for (int i = 0; i < noOfStates; i++) {
+        cout << "FOR STATE q" << i << endl;
+        vector<int> v(alphabetSize);
+        for (int j = 0; j < alphabetSize; j++) {
+            cout << "Enter output state for input " << alphabets[j] << " : ";
+            cin >> v[j];
+        }
+        dfa.push_back(v);
+    }
 
-    cout<<endl<<"GIVEN DFA :"<<endl;
-	printDFA(dfa, alphabets);
+    vector<bool> reachable = findReachableStates(dfa, 0);
+    vector<vector<int>> reachableDFA;
+    vector<int> reachableFinalStates;
+    map<int, int> stateRemap;
 
-    vector<vector<int>> minimizedDFA = minimizeDFA(dfa, final_states, noOfStates, alphabetSize);
+    int newIndex = 0;
+    for (int i = 0; i < noOfStates; i++) {
+        if (reachable[i]) {
+            stateRemap[i] = newIndex++;
+            reachableDFA.push_back(dfa[i]);
+        }
+    }
+
+    for (int i = 0; i < reachableDFA.size(); i++) {
+        for (int j = 0; j < alphabetSize; j++) {
+            reachableDFA[i][j] = stateRemap[reachableDFA[i][j]];
+        }
+    }
+
+    for (int state : final_states) {
+        if (reachable[state]) {
+            reachableFinalStates.push_back(stateRemap[state]);
+        }
+    }
+
+    cout << endl << "GIVEN DFA :" << endl;
+    printDFA(reachableDFA, alphabets);
+
+    vector<vector<int>> minimizedDFA = minimizeDFA(reachableDFA, reachableFinalStates, reachableDFA.size(), alphabetSize);
 
     cout << endl << "MINIMIZED DFA:" << endl;
     printDFA(minimizedDFA, alphabets);
